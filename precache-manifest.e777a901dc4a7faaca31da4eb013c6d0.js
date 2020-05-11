0ee03ce794f003b8f414606bcf0e6bfd3aa4d02e
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-05-11 18:07:07 +0200

    Updates

---
+++ precache-manifest.e777a901dc4a7faaca31da4eb013c6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8f4a3bba0b7088e93ad3f3d000bc7f",
+    "url": "/React-To-Do-List/index.html"
+  },
+  {
+    "revision": "ff3f6be2aef2f669020a",
+    "url": "/React-To-Do-List/static/css/main.7c47e0f2.chunk.css"
+  },
+  {
+    "revision": "3ba301ab7010da9db8b6",
+    "url": "/React-To-Do-List/static/js/2.1200f6a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-List/static/js/2.1200f6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3f6be2aef2f669020a",
+    "url": "/React-To-Do-List/static/js/main.6d3b5e2e.chunk.js"
+  },
+  {
+    "revision": "de8e4e037ceb442df209",
+    "url": "/React-To-Do-List/static/js/runtime-main.be72cb30.js"
+  }
+]);
